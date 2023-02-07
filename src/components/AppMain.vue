70ea4a84819843cc55140e570668ae41e55dfbfb
skiotto93 <fabio.forenza@hotmail.com>
2023-02-07 19:23:33 +0100

    Collegato con repo Laravel Boolfolio - API

---
+++ src/components/AppMain.vue
@@ -0,0 +1,19 @@
+<script>
+import ListProjects from './ListProjects.vue';
+export default {
+    name: "AppMain",
+    components: {
+        ListProjects,
+    }
+};
+</script>
+
+<template>
+    <main>
+        <ListProjects />
+    </main>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/ListProjects.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from "axios";
+
+export default {
+    name: "ListProjects",
+    data() {
+        return {
+            projects: [],
+        }
+    },
+    created() {
+        axios.get('http://127.0.0.1:8000/api/projects')
+        .then((res) =>{
+            console.log(res)
+        });
+    }
+};
+</script>
+
+<template>
+    <section>
+        <h1>Lista Progetti:</h1>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
