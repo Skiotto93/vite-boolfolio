710222060b4853b0f10bc74792d3bc11f195a2b3
skiotto93 <fabio.forenza@hotmail.com>
2023-02-07 23:11:45 +0100

    Creato ProjectCard

---
+++ src/components/CardProject.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: "CardProject",
+    props: {
+        data: Object,
+    }
+}
+</script>
+
+<template>
+    <section>
+        <div class="card">
+            <div class="card-body">
+                <h3>{{ data.name }}</h3>
+                <img v-if="data.image_url" :src="data.image_url" :alt="data.name">
+                <p>{{ data.description }}</p>
+                <h5>{{ data.name_client }}</h5>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+    section {
+        max-width: 1400px;
+        .card{
+            width: 400px;
+        }
+    }
+</style>
--- src/components/ListProjects.vue
+++ src/components/ListProjects.vue
@@ -1,8 +1,12 @@
 <script>
 import axios from "axios";
+import CardProject from "./CardProject.vue";
 
 export default {
     name: "ListProjects",
+    components: {
+        CardProject,
+    },
     data() {
         return {
             projects: [],
@@ -11,7 +15,7 @@ export default {
     created() {
         axios.get('http://127.0.0.1:8000/api/projects')
         .then((res) =>{
-            console.log(res)
+            this.projects = res.data;
         });
     }
 };
@@ -20,9 +24,31 @@ export default {
 <template>
     <section>
         <h1>Lista Progetti:</h1>
+        <!-- <div class="container">
+            <ol>
+                <li >
+                    {{ project.name }}
+                </li>
+            </ol>
+        </div> -->
+        <div class="container-card">
+            <ul>
+                <li class="project" v-for="project in projects">
+                    <CardProject :data="project" />
+                </li>
+            </ul>
+        </div>
     </section>
 </template>
 
 <style lang="scss" scoped>
-
+    .container-card {
+        max-width: 1200px;
+        ul {
+            display: flex;
+            li {
+                list-style: none;
+            }
+        }
+    }
 </style>
